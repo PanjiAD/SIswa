79f9b3a13b85e8050a67883e462b46f5c84e31ed
panjiad <panjiawwaludi430@gmail.com>
2019-12-13 09:49:45 +0700

    add lihat siswa

--- app/src/main/java/com/example/siswa/activites/MainActivity.java
+++ app/src/main/java/com/example/siswa/activites/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.siswa.activites;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.siswa.R;
 
@@ -12,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void handleTambahData(View view) {
+        Intent intent = new Intent(this, tambahSiswa.class);
+        startActivity(intent);
+    }
+
+    public void handleLihatData(View view) {
+        Intent intent = new Intent(this, lihatSiswa.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/siswa/activites/lihatSiswa.java
@@ -0,0 +1,16 @@
+package com.example.siswa.activites;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.siswa.R;
+
+public class lihatSiswa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lihat_siswa);
+    }
+}
